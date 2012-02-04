5aa8d96ca0201b37838051dd83c7a423f70e6661
Conn O'Griofa <connogriofa@gmail.com>
2012-02-04 00:41:14 +0000

    first commit

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+    "version": "1",
+    "homepage": "http://www.madteam.co",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=UJKH4W8TDQ6WS&lc=IE&item_name=Donations&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted",
+    "roms": [
+        {
+            "name": "CyanogenMod 7.2-RC0",
+            "summary": "Nightly Release (20120203) [test, do not install yet]",
+            "product": "gingerbread-nightly",
+            "modversion": "CyanogenMod-7-20120203-MADTEAM-U8160",
+            "incremental": "20120203",
+            "device": "u8160",
+            "url": "http://goo-inside.me/devs/psyke83/roms/u8160/update-cm-7-02032012-MADTEAM-U8160.zip",
+            "addons": [
+                {
+                    "name": "Google Apps (20110828)",
+                    "url": "http://goo-inside.me/gapps/gapps-gb-20110828-signed.zip"
+                }
+            ]
+        }
+    ]
+}
